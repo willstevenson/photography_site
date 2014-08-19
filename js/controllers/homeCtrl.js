c0d69ac962be0fff2c95c554ceda9d03eca93dcb
Zachary Lincoln <zach@burlingtoncollective.com>
2014-08-19 18:01:41 -0400

    moved html to partials

---
+++ js/controllers/homeCtrl.js
@@ -0,0 +1,3 @@
+controllers.controller('HomeCtrl', ['$scope', function($scope){
+	console.log('hello world');
+}]);
---
+++ js/main.js
@@ -0,0 +1,35 @@
+var app = angular.module('photographyApp', ['ngRoute', 'app.controllers', 'app.services', 'app.directives']);
+
+var controllers = angular.module('app.controllers', []),
+		services = angular.module('app.services', []),
+		directives = angular.module('app.directives', []);
+
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+	$routeProvider
+		.when('/', {
+			templateUrl: 'partials/home.html',
+			controller: 'HomeCtrl',
+			pageTitle: 'Home'
+		})
+		.when('/about', {
+			templateUrl: 'partials/about.html',
+			controller: 'HomeCtrl',
+			pageTitle: 'About'
+		})
+		.when('/contact', {
+			templateUrl: 'partials/contact.html',
+			controller: 'HomeCtrl',
+			pageTitle: 'Contact'
+		})
+		.when('/projects', {
+			templateUrl:'partials/projects.html',
+			controller: 'HomeCtrl',
+			pageTitle: 'Projects'
+		})
+		when('/projects/:projectName', {
+			templateUrl:'partials/project.html',
+			controller: 'HomeCtrl',
+			pageTitle: 'Projects'
+		});
+
+}])
